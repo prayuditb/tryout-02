54ea66990faa5865a13aa4f0407e84f8dfc62fb7
Prayudi Tirta Bayu <prayuditb@gmail.com>
2017-04-10 10:22:59 +0700

    add module and package native module

--- react-native/SpeechToText/android/app/src/main/java/com/speechtotext/STTmodule.java
+++ react-native/SpeechToText/android/app/src/main/java/com/speechtotext/STTmodule.java
@@ -1,76 +1,71 @@
 package com.speechtotext;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.widget.Toast;
 
-import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 import java.util.ArrayList;
-import java.util.Locale;
- 
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.os.Bundle;
-import android.speech.RecognizerIntent;
-import android.widget.Toast;
- 
 
-import java.util.Map;
+/**
+ * Created by prayuditb on 4/10/17.
+ */
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.speech.RecognizerIntent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+public class STTmodule extends ReactContextBaseJavaModule implements ActivityEventListener{
 
-import java.util.ArrayList;
-import java.util.Locale;
-
-public class STTmodule extends AppCompatActivity {
-
-    private TextView voiceInput;
-    private TextView speakButton;
     private final int REQ_CODE_SPEECH_INPUT = 100;
+    private ReactApplicationContext mReactContext;
+    private Promise mPromise;
+    private Activity mCurrentActivity;
 
-    // Showing google speech input dialog
+    public STTmodule (ReactApplicationContext reactContext){
+        super(reactContext);
+        reactContext.addActivityEventListener(this);
+        mReactContext = reactContext;
+    }
 
-    private void askSpeechInput() {
+    @Override
+    public String getName(){
+        return "SpeechToText";
+    }
+
+    @ReactMethod
+    public void start(Promise promise){
+        openSpeechToText();
+        mPromise = promise;
+    }
+
+    private void openSpeechToText(){
+         mCurrentActivity = getCurrentActivity();
+
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                "Hi speak something");
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say Something");
 
+        mCurrentActivity.startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        {
+            Toast.makeText(mReactContext, R.string.SPEECH_NOT_SUPPORT,
+                    Toast.LENGTH_SHORT).show();
+            mPromise.reject("100", "Sorry! Your device doesn\\'t support speech input");
         }
     }
 
-    // Receiving speech input
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        ArrayList<String> result = data
+                .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        mPromise.resolve(result.get(0));
+    }
 
-        switch (requestCode) {
-            case REQ_CODE_SPEECH_INPUT: {
-                if (resultCode == RESULT_OK && null != data) {
+    @Override
+    public void onNewIntent(Intent intent) {
 
-                    ArrayList<String> result = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    voiceInput.setText(result.get(0));
-                }
-                break;
-            }
-
-        }
     }
 }
